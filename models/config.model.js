0cd89192e07f955cf1b8dbbe0178cd72fd703145
legen92 <confessional111@gmail.com>
2022-09-19 14:41:29 +0700

    ok

--- models/config.model.js
+++ models/config.model.js
@@ -19,14 +19,14 @@ require("dotenv").config();
 // );
 const sequelize = new Sequelize(
   process.env.DATABASE_URL,
-  {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  }
+  // {
+  //   dialectOptions: {
+  //     ssl: {
+  //       require: true,
+  //       rejectUnauthorized: false,
+  //     },
+  //   },
+  // }
 );
 
 module.exports = sequelize;
